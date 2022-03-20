11f85d999dfbd9c51a75e072da6a19430fcfb6b8
mohamedelserafy <elserafy02@gmail.com>
2022-03-20 19:05:44 +0200

    validation pipe

--- src/item/dto/update-item.dto.ts
+++ src/item/dto/update-item.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateItemDto } from './create-item.dto';
-import { IsBoolean, IsNotEmpty } from 'class-validator';
+import { IsBoolean } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateItemDto extends PartialType(CreateItemDto) {
